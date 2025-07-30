399f9c9565e6d349832fdfc0e04011a34d4ffac5
Victor C. <vicajilau@gmail.com>
2025-07-30 14:27:37 +0200

    Added more steps

---
+++ test/widget_test.dart
@@ -0,0 +1,21 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:maso/core/service_locator.dart';
+import 'package:maso/main.dart';
+
+void main() {
+  ServiceLocator.instance.setup();
+
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MasoApp());
+
+    expect(find.text('1'), findsNothing);
+  });
+}
