d38f62011125f0d8aff32eee6b9632a211aa0dd2
Victor C. <vicajilau@gmail.com>
2025-08-01 07:07:16 +0200

    Added iOS support

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,24 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        guard url.pathExtension == "quiz" else {
+            print("File is not a QUIZ file, ignoring...")
+            return false
+        }
+
+        let controller = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "quiz.file", binaryMessenger: controller.binaryMessenger)
+        channel.invokeMethod("openFile", arguments: url.path) { result in
+            if let error = result as? FlutterError {
+                print("Failed to open file: \(error.message ?? "Unknown error")")
+            }
+        }
+        return true
+    }
 }
