8b9467af8a96572ea3e558dbb8528ee237274a15
Victor C. <vicajilau@gmail.com>
2025-08-01 06:55:23 +0200

    Added macOS support

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,11 +3,35 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    override func application(_ application: NSApplication, open urls: [URL]) {
+        for url in urls {
+            sendToFlutter(url.path)
+        }
+    }
+    
+    override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        sendToFlutter(filename)
+        return true
+    }
+    
+    override func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        for url in filenames {
+            sendToFlutter(url)
+        }
+    }
+    
+    func sendToFlutter(_ filename: String) {
+        if let controller = mainFlutterWindow?.contentViewController as? FlutterViewController {
+            let channel = FlutterMethodChannel(name: "quiz.file", binaryMessenger: controller.engine.binaryMessenger)
+            channel.invokeMethod("openFile", arguments: filename)
+        }
+    }
+    
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
 }
