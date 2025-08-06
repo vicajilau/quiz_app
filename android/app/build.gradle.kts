e5744bf6a8fd0b720d3127e31e60b37ca45e8d00
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-08-06 14:41:20 +0200

    Pin Android compileSdk and NDK versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 
 android {
     namespace = "es.victorcarreras.quiz_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
