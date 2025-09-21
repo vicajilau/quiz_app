6064eceacb1076a82a9106b2075ed013b727d065
Victor C. <34163765+vicajilau@users.noreply.github.com>
2025-09-21 10:33:52 +0200

    feat: Add raffle button to app bar with tooltip in HomeScreen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -103,6 +103,13 @@ class _HomeScreenState extends State<HomeScreen> {
             return Scaffold(
               appBar: AppBar(
                 title: Text(AppLocalizations.of(context)!.titleAppBar),
+                leading: Tooltip(
+                  message: AppLocalizations.of(context)!.raffleTooltip,
+                  child: IconButton(
+                    onPressed: _isLoading ? null : () => context.go('/raffle'),
+                    icon: const Icon(Icons.casino, color: Colors.white),
+                  ),
+                ),
                 actions: [
                   // Disable the create and load button if a file is loading
                   Tooltip(
@@ -129,16 +136,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         AppLocalizations.of(context)!.load,
                         style: const TextStyle(color: Colors.white),
                       ),
-                    ),
-                  ),
-                  // Raffle button
-                  Tooltip(
-                    message: AppLocalizations.of(context)!.raffleTooltip,
-                    child: IconButton(
-                      onPressed: _isLoading
-                          ? null
-                          : () => context.go('/raffle'),
-                      icon: const Icon(Icons.casino, color: Colors.white),
                     ),
                   ),
                   // Configuration button
