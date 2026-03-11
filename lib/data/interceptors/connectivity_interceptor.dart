72545b192afce9a515a1807dc18cacdb93009e1d
Nacchofer31 <10453558+Nacchofer31@users.noreply.github.com>
2026-03-11 16:46:41 +0100

    feature: implemented connectivity interceptor

---
+++ lib/data/interceptors/connectivity_interceptor.dart
@@ -0,0 +1,62 @@
+// Copyright (C) 2026 Víctor Carreras
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:quizdy/domain/models/custom_exceptions/connectivity_exception.dart';
+
+/// Global Dio interceptor that converts connectivity-related [DioException]s
+/// into [ConnectivityException] so callers can handle network availability
+/// uniformly without depending on Dio types.
+class ConnectivityInterceptor extends Interceptor {
+  static const _connectivityTypes = {
+    DioExceptionType.connectionError,
+    DioExceptionType.connectionTimeout,
+    DioExceptionType.sendTimeout,
+    DioExceptionType.receiveTimeout,
+  };
+
+  static const _abortedMessages = [
+    'Software caused connection abort',
+    'Connection closed',
+  ];
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final isConnectivityError =
+        _connectivityTypes.contains(err.type) ||
+        (err.type == DioExceptionType.unknown && err.error is SocketException);
+
+    if (!isConnectivityError) {
+      super.onError(err, handler);
+      return;
+    }
+
+    final errorStr = err.error?.toString() ?? '';
+    final isAborted = _abortedMessages.any(errorStr.contains);
+    final type = isAborted
+        ? ConnectivityExceptionType.connectionAborted
+        : ConnectivityExceptionType.noInternet;
+
+    handler.reject(
+      DioException(
+        requestOptions: err.requestOptions,
+        error: ConnectivityException(type),
+        type: err.type,
+      ),
+    );
+  }
+}
