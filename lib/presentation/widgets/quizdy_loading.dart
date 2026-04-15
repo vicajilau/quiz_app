58628a9cecdaebeede49e7543ad23c2e49de4a3d
Nacchofer31 <10453558+Nacchofer31@users.noreply.github.com>
2026-04-15 10:54:26 +0200

    chore: added missing `LICENSE`

--- lib/presentation/widgets/quizdy_loading.dart
+++ lib/presentation/widgets/quizdy_loading.dart
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 Victor Carreras
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -8,11 +23,6 @@ class QuizdyLoading extends StatelessWidget {
   final double? width;
 
   @override
-  Widget build(BuildContext context) {
-    return Lottie.asset(
-      'images/quizdy_loading.json',
-      height: height,
-      width: width,
-    );
-  }
+  Widget build(BuildContext context) =>
+      Lottie.asset('images/quizdy_loading.json', height: height, width: width);
 }
