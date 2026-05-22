c58d457f251923ebbde45b0cfad0c335ec41210c
Victor C. <34163765+vicajilau@users.noreply.github.com>
2026-05-22 16:29:02 +0200

    feat: add ApplicationCategory to release notes payload for Microsoft Store

--- distribution/windows/generate_windows_release_notes.dart
+++ distribution/windows/generate_windows_release_notes.dart
@@ -76,7 +76,10 @@ void main(List<String> args) {
     };
   }
 
-  final Map<String, Object> payload = <String, Object>{'Listings': listings};
+  final Map<String, Object> payload = <String, Object>{
+    'ApplicationCategory': 'Education_StudyAids',
+    'Listings': listings,
+  };
 
   outputFile.parent.createSync(recursive: true);
   outputFile.writeAsStringSync(
